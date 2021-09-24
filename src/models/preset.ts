fff98958d4175b2d218298ce20c7d4ca2f81a284
Jabun <jabun@deepfi.xyz>
2021-09-24 13:48:57 -0400

    Preset 20 working

--- src/models/preset.ts
+++ src/models/preset.ts
@@ -23,22 +23,25 @@ export default class Preset {
                 if (addressBalances.assetsPercentages.hasOwnProperty('nft')) {
                     addressBalances.assetsPercentages['nft'] = {};
                 }
+
                 for (let [type, values] of Object.entries(addressBalances.assetsPercentages)) {
                     var sortable = [];
-                    for (var key in values){
+                    for (let key in values){
                         if (values.hasOwnProperty(key)){
-                            sortable.push([key, values[key]]); // each item is an array in format [key, value]
+                            sortable.push([key, values[key]]);
                         }
                     }
                     const sortedType = sortable.sort((a: any, b: any) => b[1].percentage - a[1].percentage)
                     let index = 0
-                    let test = addressBalances.assetsPercentages[type]
                     for(let asset of sortedType){
                         let name = asset[0]
-                        test[`${name}`].percentage = index
+                        addressBalances.assetsPercentages[type][`${name}`].percentage = index
                         index ++
                     }
                 }
+
+                let test = addressBalances.assetsPercentages
+
                 break;
 
             default:
