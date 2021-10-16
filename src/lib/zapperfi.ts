e6714a65a9b3adabd97ecaba825b7c669c13cd40
Oleg Petrov <oleg.petrov.eth@gmail.com>
2021-10-16 11:16:43 -0400

    Clean up lib ZapperFI

--- src/lib/zapperfi.ts
+++ src/lib/zapperfi.ts
@@ -13,14 +13,8 @@ import {
     getEmptyBalances,
     getEmptyAssets,
     getAssetCategories,
-    addAsset,
-    addToken,
-    extractAssetImg
+    addAsset
 } from './helpers';
-
-// gauge & single-staking return assets classified as non staking (pool, base, deposit, etc...)
-// they are still accounted for in staking since most of them are not listed in general balance route
-const SUPPORTED_STAKING_PROTOCOLS = ['masterchef', 'gauge', 'single-staking'];
 
 class ZapperApi {
     private static apiKey?: string = process.env.ZAPPERFI_API_KEY;
@@ -60,9 +54,6 @@ class ZapperApi {
         let assets = getEmptyAssets();
 
         try {
-            // let stakingBalance: number;
-            // let stakingTokens: IToken[];
-            // let claimableTokens: IToken[];
 
             let uniqueProtocols: any = await ZapperApi.getSupportedProtocols(disguise); // TODO: how to handle returned type Promise<string[]> doesn't work
             let promises = ZapperApi.balancePromiseGenerator(disguise, uniqueProtocols);
@@ -103,19 +94,6 @@ class ZapperApi {
                 }
             }
 
-            // seems not needed anymore since feractor on 07/10/2021 from zapper
-            // special attention kid and needs its own dedicated route
-            // [stakingBalance, stakingTokens, claimableTokens] = await ZapperApi.getStakingBalances(disguise);
-            // balances[AssetCategories.staking] = stakingBalance;
-
-            // for(let stakingToken of stakingTokens) {
-            //     addToken(assets, AssetCategories.staking, stakingToken);
-            // }
-
-            // for(let claimableToken of claimableTokens) {
-            //     addToken(assets, AssetCategories.claimable, claimableToken);
-            // }
-
             let addressBalances = new AddressBalances(balances, assets, disguise.options);
             let preset = new Preset(disguise.preset);
             preset.filter(addressBalances);
@@ -130,84 +108,6 @@ class ZapperApi {
             console.log(`[zapperFi.getBalances]: ${e}`);
             throw e;
         }
-    }
-
-    // masterchef, gauge, single-staking
-    static async getStakingBalances(disguise: Disguise): Promise<[number, IToken[], IToken[]]> {
-        let stakingBalance: number = 0;
-        let claimableTokens: IToken[] = [];
-        let stakingTokens: IToken[] = [];
-
-        try {
-            let promises = ZapperApi.stakingBalancePromiseGenerator(disguise);
-            let responses = await Promise.all(promises);
-
-            for(let response of responses) {
-                let stakingList = response.data[disguise.address];
-
-                if(stakingList && stakingList.length > 0) {
-                    for(let staking of stakingList) {
-                        stakingBalance += parseFloat(staking.balanceUSD);
-
-                        // add claimable tokens
-                        if(staking.rewardTokens) {
-                            for(let rewardToken of staking.rewardTokens) {
-                                claimableTokens.push({
-                                    address: rewardToken.address,
-                                    symbol: rewardToken.symbol,
-                                    balance: rewardToken.balanceUSD,
-                                    protocol: rewardToken.protocolDisplay || '',
-                                    label: rewardToken.label || rewardToken.symbol,
-                                    img: extractAssetImg(rewardToken, 'base')
-                                });
-                            }
-                        }
-
-                        let assetTokens = staking.tokens;
-                        if(assetTokens && assetTokens.length > 0) {
-                            for(let assetToken of assetTokens) {
-                                assetToken.img = extractAssetImg(assetToken, stakingList.category);
-                                delete assetToken.address;
-                                delete assetToken.balance;
-                                delete assetToken.balanceUSD;
-                                delete assetToken.decimals;
-                                delete assetToken.price;
-                                delete assetToken.type;
-                            }
-                        }
-
-                        // add stacking position with tokens for images
-                        stakingTokens.push({
-                            address: staking.address,
-                            symbol: staking.symbol,
-                            balance: staking.balanceUSD,
-                            protocol: staking.protocolDisplay || '',
-                            label: staking.label || staking.symbol,
-                            tokens: assetTokens
-                        });
-                    }
-                }
-            }
-
-            return [stakingBalance, stakingTokens, claimableTokens];
-        } catch(e) {
-            console.log(`[zapperFi.getStakingBalances]: ${e}`);
-            return [0, [], []];
-        }
-    }
-
-    private static stakingBalancePromiseGenerator(disguise: Disguise) {
-        let promises = [];
-        let params = new URLSearchParams();
-        params.append('addresses[]', disguise.address);
-        params.append('api_key', ZapperApi.apiKey || '');
-
-        for(let stakingProtocol of SUPPORTED_STAKING_PROTOCOLS) {
-            let url = `${ZapperApi.apiUrl}/staked-balance/${stakingProtocol}?` + params.toString();
-            promises.push(axios.get(url));
-        }
-
-        return promises;
     }
 
     private static balancePromiseGenerator(disguise: Disguise, protocols: any) { // find TS compliant solutions to interface protocols
