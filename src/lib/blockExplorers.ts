aadc528fc7eb90c823d417f052d7f8a772e7640b
Jabun <jabun@deepfi.xyz>
2021-12-14 12:51:27 -0500

    Gas Stats

--- src/lib/blockExplorers.ts
+++ src/lib/blockExplorers.ts
@@ -15,8 +15,10 @@ class BlockExplorersAPI {
         let txsStats: any = {}
         let totalTxs: number = 0
         let totalERC721Txs: number = 0
+        let totalFailedTxs: number = 0
         let totalTxsCost: number = 0
         let totalERC721TxsCost: number = 0
+        let totalFailedTxsCost: number = 0
 
         try{
             for (let chain of chains) {
@@ -32,9 +34,13 @@ class BlockExplorersAPI {
                     await timeout(1000);
                     // let normalTxs = await ZapperApi.getNormalTxs(addresses, chain)
                     let erc721Txs = await BlockExplorersAPI.getERC721Txs(addresses, apiUrl, apiKey)
+
+                    let failedTxs = normalTxs.filter((tx: any) => tx.isError == '1')
                     
                     let normalTxsGasSpent: any
                     let erc721TxsGasSpent: any
+                    let failedTxsGasSpent: any
+
                     let gasPrice: any
                     if(normalTxs.length > 0){
                         totalTxs += normalTxs.length
@@ -48,6 +54,12 @@ class BlockExplorersAPI {
                         erc721TxsGasSpent = await extractEtherscanGas(erc721Txs)
                     } else {
                         erc721TxsGasSpent = 0
+                    }
+                    if(failedTxs.length > 0){
+                        totalFailedTxs += failedTxs.length
+                        failedTxsGasSpent = await extractEtherscanGas(failedTxs)
+                    } else {
+                        failedTxsGasSpent = 0
                     }
                     
                     
@@ -64,22 +76,87 @@ class BlockExplorersAPI {
                     if(normalTxs.length > 0){
                         totalTxsCost += (normalTxsGasSpent * gasPrice)
                     }
+                    if(failedTxs.length > 0){
+                        totalFailedTxsCost += (failedTxsGasSpent * gasPrice)
+                    }
 
                     txsStats[chain] = {
                         normalTxsAmount: normalTxs.length,
                         erc721TxsAmount: erc721Txs.length,
+                        failedTxsAmount: failedTxs.length,
                         normalTxsGasSpent: normalTxsGasSpent,
                         erc721TxsGasSpent: erc721TxsGasSpent,
+                        failedTxsGasSpent: failedTxsGasSpent,
                         normalTxGasCostUSD: (normalTxsGasSpent * gasPrice),
                         erc721TxGasCostUSD: (erc721TxsGasSpent * gasPrice),
+                        failedTxsGasCostUSD: (failedTxsGasSpent * gasPrice),
                         gasPrice: gasPrice
                     }
                 }
             }
             txsStats.totalTxs = totalTxs
             txsStats.totalERC721Txs = totalERC721Txs
+            txsStats.totalFailedTxs = totalFailedTxs
             txsStats.totalTxsCost = totalTxsCost
             txsStats.totalERC721TxsCost = totalERC721TxsCost
+            txsStats.totalFailedTxsCost = totalFailedTxsCost
+
+            txsStats = {
+                ...txsStats,
+                gasSpentDistribution: {
+                    ethereum : {
+                        title: 'Ethereum',
+                        color: "#ccaff8",
+                        percentage: txsStats.ethereum.normalTxGasCostUSD/totalTxsCost * 100,
+                        amount: txsStats.ethereum.normalTxGasCostUSD
+                    },
+                    polygon : {
+                        title: 'Polygon',
+                        color: "#8247e5",
+                        percentage: txsStats.polygon.normalTxGasCostUSD/totalTxsCost * 100,
+                        amount: txsStats.polygon.normalTxGasCostUSD
+                    },
+                    bsc: {
+                        title: 'BSC',
+                        color: '#FBDA3C',
+                        percentage: txsStats['binance-smart-chain'].normalTxGasCostUSD/totalTxsCost * 100,
+                        amount: txsStats['binance-smart-chain'].normalTxGasCostUSD
+                    }
+                },
+                txsDistribution: {
+                    ethereum : {
+                        title: 'Ethereum',
+                        color: "#ccaff8",
+                        percentage: txsStats.ethereum.normalTxsAmount / totalTxs * 100,
+                        amount: txsStats.ethereum.normalTxsAmount
+                    },
+                    polygon : {
+                        title: 'Polygon',
+                        color: "#8247e5",
+                        percentage: txsStats.polygon.normalTxsAmount / totalTxs * 100,
+                        amount: txsStats.polygon.normalTxsAmount
+                    },
+                    bsc: {
+                        title: 'BSC',
+                        color: '#FBDA3C',
+                        percentage: txsStats['binance-smart-chain'].normalTxsAmount / totalTxs * 100,
+                        amount: txsStats['binance-smart-chain'].normalTxsAmount
+                    }
+                }
+            }
+
+            if(txsStats.ethereum.normalTxsAmount < 1){
+                delete txsStats.gasSpentDistribution.ethereum
+                delete txsStats.txsDistribution.ethereum
+            }
+            if(txsStats.polygon.normalTxsAmount < 1){
+                delete txsStats.gasSpentDistribution.polygon
+                delete txsStats.txsDistribution.polygon
+            }
+            if(txsStats["binance-smart-chain"].normalTxsAmount < 1){
+                delete txsStats.gasSpentDistribution.bsc
+                delete txsStats.txsDistribution.bsc
+            }
             
             return txsStats
         }catch(e){
